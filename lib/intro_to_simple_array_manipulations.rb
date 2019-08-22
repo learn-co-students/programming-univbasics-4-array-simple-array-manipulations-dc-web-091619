c6542a56732e6206b973f3459f6532d82c727015
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-22 21:29:07 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,4 +30,5 @@ def using_concat (array1, array2)
   array1 = ["raindrops on roses", "whiskers on kittens"]
   array2 = ["sports cars", "flatiron school"]
   array1.concat(array2)
+  array.length > 2
 end

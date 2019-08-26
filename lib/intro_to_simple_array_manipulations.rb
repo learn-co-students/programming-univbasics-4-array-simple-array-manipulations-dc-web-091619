d310c2cbc00d5b9a800728fe4381bc9b716c4b70
Charles DeLeo <charles.deleo979@gmail.com>
2019-08-26 21:32:40 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,19 @@ end
 def using_insert (array, element)
   array.insert(4, element)
 end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten 
+end
+
+def using_delete (array, string)
+  array.delete() 
+end
+
+def using_delete_at (array, integer)
+  array.delete_at()
+end
